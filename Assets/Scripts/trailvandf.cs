ca1beab2fc48c6e33e3f597b41691bddb3814ba8
Maddisen Gumeson <trackstar800@gmail.com>
2015-09-02 10:02:51 -0600

    Variables and Functions

---
+++ Assets/Scripts/trailvandf.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trailvandf : MonoBehaviour {
+	int mgint = 8;
+	// Use this for initialization
+	void Start () {
+		mgint = MultiplyByNine (mgint);
+		Debug.Log (mgint);
+	
+	}
+	int MultiplyByNine(int number){
+		int ret;
+
+		ret = number * 9;
+		return ret;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
