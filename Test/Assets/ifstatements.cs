96157fa55cba3dc2c0ed3e182d3f547087ca4d62
Maddisen Gumeson <trackstar800@gmail.com>
2015-09-23 09:10:17 -0600

    If Statements Assignments

---
+++ Test/Assets/ifstatements.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifstatements : MonoBehaviour {
+	public float toobright = 60.0f;
+	public float lightquality = 90.0f;
+	public float toodark = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+			LightTest ();
+
+		lightquality -= Time.deltaTime * 8.0f;
+	}
+	void LightTest ()
+	{
+
+		if (lightquality > toobright) 
+		{
+			Debug.Log("It's Too Bright.");
+		}
+		else if (lightquality < toodark) 
+		{
+			Debug.Log("It's Too Dark.");
+		} 
+		else 
+		{
+			Debug.Log("It's Perfect Lighting.");
+		}
+	}
+}
---
+++ Test/Assets/ifstatements2.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifstatements2 : MonoBehaviour {
+	public float TooBright = 60.0f;
+	public float LightQuality = 90.0f;
+	public float TooDark = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+			LightTest ();
+		LightQuality -= Time.deltaTime * 8.0f;
+	}
+	void LightTest ()
+	{
+		if (LightQuality > TooBright) {
+			Debug.Log ("It's Too Bright");
+		} else if (LightQuality < TooDark) {
+			Debug.Log ("It's Too Dark");
+		} else {
+			Debug.Log ("It's Perfect Lighting");
+		}
+	
+	}
+}
