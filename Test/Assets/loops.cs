e424ef47bdd80f398c2d05176d03ac61579d4e53
Maddisen Gumeson <trackstar800@gmail.com>
2015-10-05 09:34:15 -0600

    commit

---
+++ Test/Assets/loops.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loops : MonoBehaviour {
+	int cupsinthesink = 4;
+	int numenemies = 3;
+	// Use this for initialization
+	void Start () {
+	
+	while (cupsinthesink > 0) {
+			Debug.Log ("I've washed a cup!");
+			cupsinthesink--;
+		}
+	
+		bool shouldcontinue = false;
+		do {
+			print ("should continue");
+		} while (shouldcontinue == true);
+
+		for (int i=0; i < numenemies; i++) {
+			Debug.Log ("I've created enemy number: " + i);
+		}
+
+		string[] strings = new string[3];
+		strings [0] = "first string";
+		strings [1] = "second string";
+		strings [2] = "third string";
+		foreach (string item in strings) {
+			print (item);
+		}
+
+	    
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
