934819e3897da5a47ec34d8f85a69ed0a8df3600
Maddisen Gumeson <trackstar800@gmail.com>
2015-11-23 14:58:18 -0700

    imported sprites for duck hunt

---
+++ Test/Assets/duckspawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class duckspawn : MonoBehaviour {
+	public GameObject duck;
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+	public void SpawnDuck ()
+	{
+		Instantiate(duck, transform.position, Quaternion.identity);
+	}
+}
