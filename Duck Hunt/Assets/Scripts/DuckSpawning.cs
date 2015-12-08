0e0fc702158b92678460da82f5049e5b998e2a44
Maddisen Gumeson <trackstar800@gmail.com>
2015-12-07 23:38:25 -0700

    Working Sprite Assignment

---
+++ Duck Hunt/Assets/Scripts/DuckSpawning.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckSpawning : MonoBehaviour {
+
+	public GameObject Duck;
+	public float spawnTime = 6f;
+	public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnDuck", spawnTime, spawnTime);
+	}
+
+	void SpawnDuck()
+	{
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		Instantiate (Duck, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+}
